dbcce05f3b0b2ab35b5235b69a1a376be7fa4100
kalamaboop <rosariok28@yahoo.com>
2015-01-20 15:50:37 -0800

    Update playingCards.js

--- js/playingCards.js
+++ js/playingCards.js
@@ -162,8 +162,8 @@ if (Array.indexOf === undefined) {
     if (a.rank == b.rank)       return 0;
     if (a.rank == "N")        return 1;
     if (b.rank == "N")        return -1;
-    if (a.rank == "A")        return 0;
-    if (b.rank == "A")        return 0;
+    if (a.rank == "A")        return -1;
+    if (b.rank == "A")        return -1;
     if (!isNaN(a.rank - b.rank))    return a.rank - b.rank;
     if (a.rank == "K" && b.rank == "J")   return 1;
     if (a.rank == "J" && b.rank == "K")   return -1;
