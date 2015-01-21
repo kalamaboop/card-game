0489951fa0974f622e6913355175a22632d33cd4
kalamaboop <rosariok28@yahoo.com>
2015-01-20 16:21:10 -0800

    Update playCards.js

--- js/playCards.js
+++ js/playCards.js
@@ -61,6 +61,7 @@ $(document).ready(function(){
     });
     $('#orderByRank').click(doOrderByRank);
     $('#orderBySuit').click(doOrderBySuit);
+    $('#orderByRankAndSuit').click(doOrderByRankAndSuit)
 
 });
 /*
