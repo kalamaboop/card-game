34000b7aa174ed327011ccb424bf926989a0135e
kalamaboop <rosariok28@yahoo.com>
2015-02-12 13:49:44 -0800

    Update playCards.js

--- js/playCards.js
+++ js/playCards.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
     var hand = [];
     var computerHand = [];
-    var discardPile = $("#discardPile")
+    var discardPile = [];
     
     var showError = function(msg){
         $('#error').html(msg).show();
